5567a7a0e35777496a67311044a5f18952701102
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-11-21 19:28:09 +0530

    Check employee is present or not

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,5 +2,13 @@ import java.util.*;
 class EmployeeWage{
 	public static void main(String[]args){
 		System.out.println("Welcome");
+		int isPresent = 1;
+		int checkAtten = Math.floor(Math.random()*10);
+		if(checkAtten == isPresent){
+			System.out.println("Present");
+		}
+		else{
+			System.out.println("Absent");
+		}
 	}
 }
