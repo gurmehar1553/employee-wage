dede5a4533b359ec4b41a20dd795bf56874eee95
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-11-21 23:08:09 +0530

    Done

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,6 +1,16 @@
 import java.util.*;
 class EmployeeWage{
 	public static void main(String[]args){
+		
+		int isPresent = 1;
+		double checkAtten = Math.floor(Math.random()*10) % 2;
+		if(checkAtten == isPresent){
+			System.out.println("Employee is Present");
+		}
+		else{
+			System.out.println("Employee is Absent");
+		}
+
 		int wagePerHr = 20;
 
 		int fullDayHrs = 8;
@@ -12,6 +22,10 @@ class EmployeeWage{
 		System.out.println("Part Time Employee's Wage :" + partTimeWage);
 
 		int workDays = 20;
+
+		int monthlyWage = workDays * dailyEmpWage;
+		System.out.println("Monthly Employee's Wage :" + monthlyWage);
+
 		int workHrs = fullDayHrs * workDays;
 		if(workHrs > 100){
 			workHrs=100;
