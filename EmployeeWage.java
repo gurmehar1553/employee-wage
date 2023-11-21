b1ff490b01d74dfa18deae6ca2d10183025748f2
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-11-21 22:56:42 +0530

    Updated code

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,8 +2,14 @@ import java.util.*;
 class EmployeeWage{
 	public static void main(String[]args){
 		int wagePerHr = 20;
+<<<<<<< .merge_file_a15112
 		int fullDayHrs = 8;
 		int dailyEmpWage = wagePerHr * fullDayHrs;
 		System.out.println("Daily Employee's Wage :" + dailyEmpWage);
+=======
+		int partTimeHrs = 2;
+		int partTimeWage = wagePerHr * partTimeHrs;
+		System.out.println("Part Time Employee's Wage :" + partTimeWage);
+>>>>>>> .merge_file_a21320
 	}
 }
