4c39721638b6763111ce5a23a6e6775a002fcf85
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-11-21 19:44:44 +0530

    UC1 updated

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,14 +1,13 @@
 import java.util.*;
 class EmployeeWage{
 	public static void main(String[]args){
-		System.out.println("Welcome");
 		int isPresent = 1;
-		int checkAtten = Math.floor(Math.random()*10);
+		double checkAtten = Math.floor(Math.random()*10) % 2;
 		if(checkAtten == isPresent){
-			System.out.println("Present");
+			System.out.println("Employee is Present");
 		}
 		else{
-			System.out.println("Absent");
+			System.out.println("Employee is Absent");
 		}
 	}
 }
