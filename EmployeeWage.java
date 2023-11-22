e141f55af4dc4bf0f14d72da621b59961bdb7ef0
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-11-22 14:29:48 +0530

    Refracted: UC2 daily wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,8 +1,22 @@
 import java.util.*;
 class EmployeeWage{
 	public static void main(String[]args){
+		int isPresent = 1;
+		double checkAtten = Math.floor(Math.random()*10) % 2;
+
 		int wagePerHr = 20;
 		int fullDayHrs = 8;
+
+		if(checkAtten == isPresent){
+			fullDayHrs = 8;
+			System.out.println("Employee is Present");
+		}
+		else{
+			fullDayHrs = 0;
+			System.out.println("Employee is Absent");
+		}
+		
+		
 		int dailyEmpWage = wagePerHr * fullDayHrs;
 		System.out.println("Daily Employee's Wage :" + dailyEmpWage);
 	}
