01fe88368d2d7942d43ff8a1dbf2ad2f65803576
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-11-23 19:20:24 +0530

    Refracted code

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,17 +1,19 @@
 import java.util.*;
 class EmployeeWage{
+
+	public final static int IS_FULL_TIME = 2;
+	public final static int IS_PART_TIME = 3;
+	public final static int WORK_DAYS_MONTH = 20;
+	public final static int MAX_HRS = 100;
+
 	public static void main(String[]args){
-
-		int IS_FULL_TIME = 2;
-		int IS_PART_TIME = 3;
-
 		int wagePerHr = 20;
 		int numHrs=0;
 		int maxDays =0;
 		int totalHrs=0;
-		int WORK_DAYS_MONTH = 20;
+		
 		int monthlyWage = 0;
-		while(totalHrs <= 100 && maxDays <= 20){
+		while(totalHrs <= MAX_HRS && maxDays <= WORK_DAYS_MONTH){
 			int checkAtten = (int)Math.floor(Math.random()*10) % 3;
 
 			if(checkAtten == IS_FULL_TIME){
@@ -26,6 +28,7 @@ class EmployeeWage{
 			totalHrs += numHrs;
 			int dailyEmpWage = wagePerHr * numHrs;
 			monthlyWage += dailyEmpWage; 
+			maxDays++;
 			
 		}
 		
