e62dd37aa7c499b8ed2938a84f1b7b28c91ee2f7
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-11-23 13:41:29 +0530

    Refracted UC5

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -7,24 +7,25 @@ class EmployeeWage{
 
 		int wagePerHr = 20;
 		int numHrs;
+		
+		int WORK_DAYS_MONTH = 20;
+		int monthlyWage = 0;
+		for(int i=0;i<WORK_DAYS_MONTH;i++){
+			int checkAtten = (int)Math.floor(Math.random()*10) % 3;
 
-		int checkAtten = (int)Math.floor(Math.random()*10) % 3;
-
-		if(checkAtten == IS_FULL_TIME){
-			numHrs = 8;
-			System.out.println("Employee is FullTime");
-		}
-		else if(checkAtten == IS_PART_TIME){
-			numHrs = 2;
-			System.out.println("Employee is PartTime");
-		}
-		else{
-			numHrs = 0;
-			System.out.println("Employee is Absent");
+			if(checkAtten == IS_FULL_TIME){
+				numHrs = 8;
+			}
+			else if(checkAtten == IS_PART_TIME){
+				numHrs = 2;
+			}
+			else{
+				numHrs = 0;
+			}
+			int dailyEmpWage = wagePerHr * numHrs;
+			monthlyWage += dailyEmpWage; 
 		}
 		
-		
-		int dailyEmpWage = wagePerHr * numHrs;
-		System.out.println("Daily Employee's Wage :" + dailyEmpWage);
+		System.out.println("Monthly Employee's Wage :" + monthlyWage);
 	}
 }
